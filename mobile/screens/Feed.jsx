bfc826bba419d647e600ad8845bc57225999d275
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-19 17:01:21 -0500

    Feed
    - post
    - Create Post Button
    	- Redirects to PostForm

--- mobile/screens/Feed.jsx
+++ mobile/screens/Feed.jsx
@@ -1,39 +1,59 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Surface, Text } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  Surface,
+  Text,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+} from "react-native-paper";
+import Post from "./Post";
+import { useNavigation } from "@react-navigation/native";
+import { ScreenRoutes } from "../ScreenRoutes";
 
 export default function Feed(props) {
+  /*TODO: Call backend API to populate all posts*/
+  const posts = [
+    {
+      _id: 1,
+      title: "Post 1",
+      description: "This is post 1",
+      address: "1000 Location Pkway, Austin, TX",
+      author: "author 1",
+      comments: [
+        {
+          title: "comment 1",
+        },
+      ],
+    },
+    {
+      _id: 2,
+      title: "Post 2",
+      description: "This is post 2",
+      address: "2000 Location Dr, Houston, TX",
+      author: "author 2",
+      comments: [
+        {
+          title: "comment 1",
+          title: "comment 2",
+        },
+      ],
+    },
+  ];
+
   return (
     <Layout navigation={props.navigation}>
-      <Text>Feed Page</Text>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Pellentesque nec nam
-        aliquam sem et tortor. Duis at consectetur lorem donec massa sapien.
-        Dictum fusce ut placerat orci nulla. Feugiat in fermentum posuere urna.
-        Enim tortor at auctor urna nunc id cursus. Adipiscing elit pellentesque
-        habitant morbi tristique senectus et netus. Quis hendrerit dolor magna
-        eget. Velit egestas dui id ornare arcu. Quisque id diam vel quam
-        elementum pulvinar etiam non quam. Mi sit amet mauris commodo quis.
-        Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget.
-        Tortor pretium viverra suspendisse potenti nullam. Amet mauris commodo
-        quis imperdiet massa tincidunt nunc pulvinar sapien. Eu facilisis sed
-        odio morbi quis commodo odio. Scelerisque purus semper eget duis at
-        tellus at. Consequat mauris nunc congue nisi vitae suscipit tellus
-        mauris. Faucibus turpis in eu mi bibendum neque egestas. Adipiscing
-        commodo elit at imperdiet dui. Justo donec enim diam vulputate ut
-        pharetra. Urna nunc id cursus metus aliquam eleifend mi in nulla. Etiam
-        sit amet nisl purus in mollis nunc. Laoreet sit amet cursus sit amet
-        dictum sit. Risus feugiat in ante metus. Platea dictumst quisque
-        sagittis purus sit amet volutpat. Ut sem viverra aliquet eget sit amet
-        tellus cras. Pretium aenean pharetra magna ac placerat vestibulum lectus
-        mauris. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus
-        a. Nam aliquam sem et tortor. Quisque non tellus orci ac auctor augue
-        mauris augue neque. Mattis ullamcorper velit sed ullamcorper morbi
-        tincidunt ornare. Felis eget velit aliquet sagittis id consectetur purus
-      </Text>
+      <Button
+        mode="contained"
+        compact
+        onPress={() => props.navigation.navigate(ScreenRoutes.PostForm)}
+      >
+        Create New Post
+      </Button>
+      {posts.map((post) => (
+        <Post key={post._id} data={post} {...props} />
+      ))}
     </Layout>
   );
 }

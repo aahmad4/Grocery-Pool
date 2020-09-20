8214b5dab1b8cea84941546c5a9cb670807af1a5
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-20 16:50:25 -0500

    checkAuthStatus
    - Removed dummy user (testing authentication with cache support)

--- mobile/utils/checkAuthStatus.js
+++ mobile/utils/checkAuthStatus.js
@@ -1,18 +1,7 @@
 import { AsyncStorage } from "react-native";
 
 export const checkAuthStatus = async (setIsAuthenticated) => {
-  /**
-   * Temporary setting up the user in async storage, DONT DO THIS EVER
-   */
-  await AsyncStorage.setItem(
-    "@user",
-    JSON.stringify({
-      _id: "1",
-      email: "adam@smith.com",
-      name: "Adam Smith",
-      address: "Coney Island, NY",
-    })
-  );
+  await AsyncStorage.setItem("@user", {});
 
   try {
     const user = await AsyncStorage.getItem("@user");
